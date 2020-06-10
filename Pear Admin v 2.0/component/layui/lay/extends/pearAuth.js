6d0d7db24197892bde8ffe6a83ccd35f69f955dc
Jmysy <854085467@qq.com>
2020-06-11 04:20:47 +0800

    新增权限模块 待完善

---
+++ Pear Admin v 2.0/component/layui/lay/extends/pearAuth.js
@@ -0,0 +1,53 @@
+layui.define(['table', 'jquery', 'element'], function (exports) {
+    "use strict";
+
+    var MOD_NAME = 'pearAuth',
+        $ = layui.jquery,
+		element = layui.element;
+		
+    var pearAuth = function () {
+        
+		
+		
+		
+    };
+	
+	/**
+	 * 初始化
+	 * */
+	pearFrame.prototype.initPermission = function (url) {
+	  
+	    
+	} 
+	
+	/**
+	 * 新增权限
+	 * */
+	pearFrame.prototype.setPermission = function (opt) {
+	  
+	} 
+	
+	/**
+	 * 清楚权限
+	 * */
+	pearFrame.prototype.clearPermission = function (opt) {
+	    
+	} 
+	
+	/**
+	 * 删除权限
+	 * */
+	pearFrame.prototype.removePermission = function (opt) {
+	    
+	} 
+	
+	/** 
+	 * 权限验证
+	 * */
+	
+
+    
+	
+	
+	exports(MOD_NAME,new pearAuth());
+})
