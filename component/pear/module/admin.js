77db9f2f36fbf58afac4ec9e6e0e125173cb12c2
就眠仪式 <854085467@qq.com>
2020-07-23 04:41:52 +0800

    主题配置

--- component/pear/module/admin.js
+++ component/pear/module/admin.js
@@ -23,6 +23,7 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 				this.bodyRender(option);
 				this.keepLoad(option);
 				this.logoRender(option);
+				this.themeRender(option);
 			}
 
 			this.logoRender = function(option) {
@@ -139,10 +140,85 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 			this.keepLoad = function(option) {
 
 				compatible()
-
 				setTimeout(function() {
+
 					$(".loader-main").fadeOut(option.done);
 				}, option.keepLoad)
+			}
+
+			this.themeRender = function(option) {
+				var colorId = localStorage.getItem("theme-color");
+				var menu = localStorage.getItem("theme-menu");
+
+				this.colorSet(getColorById(colorId).color);
+				this.menuSkin(menu);
+			}
+
+			this.menuSkin = function(theme) {
+				$(".pear-admin").removeClass("light-theme");
+				$(".pear-admin").removeClass("dark-theme");
+				$(".pear-admin").removeClass("night-theme");
+				$(".pear-admin").addClass(theme);
+			}
+
+			this.colorSet = function(color) {
+
+				var style = '';
+
+				// 自 定 义 菜 单 配 色
+				style +=
+					'.light-theme .pear-nav-tree .layui-this a:hover,.light-theme .pear-nav-tree .layui-this,.light-theme .pear-nav-tree .layui-this a,.pear-nav-tree .layui-this a,.pear-nav-tree .layui-this{background-color: ' +
+					color + '!important;}';
+
+				// 自定义 Logo 标题演示
+				style +=
+					'.pear-admin .layui-logo .title{color:' +
+					color + '!important;}';
+
+				// 自 定 义 标 签 配 色
+				style += '.pear-frame-title .dot,.pear-tab .layui-this .pear-tab-active{background-color: ' + color +
+					'!important;}';
+
+				// 自 定 义 快 捷 菜 单
+				style += '.bottom-nav li a:hover{background-color:' +
+					color + '!important;}';
+
+				// 自 定 义 顶 部 配 色
+				style += '.pear-admin .layui-header .layui-nav .layui-nav-bar{background-color: ' + color + '!important;}'
+
+				// 自 定 义 加 载 配 色
+				style += '.ball-loader>span,.signal-loader>span {background-color: ' + color + '!important;}';
+
+				// 自 定 义 顶 部 配 色
+				style += '.layui-header .layui-nav-child .layui-this a{background-color:' + color +
+					'!important;color:white!important;}';
+
+				// 自 定 义 加 载 配 色
+				style += '#preloader{background-color:' + color + '!important;}';
+
+
+				// 自 定 义 样 式 选 择 边 框 配 色
+				style +=
+					'.pearone-color .color-content li.layui-this:after, .pearone-color .color-content li:hover:after {border: ' +
+					color + ' 2px solid!important;}';
+
+				style += '.layui-nav .layui-nav-child dd.layui-this a, .layui-nav-child dd.layui-this{background-color:' + color +
+					'!important}';
+
+				style += '.pear-social-entrance {background-color:' + color + '!important}';
+
+				style += '.pear-admin .pe-collaspe {background-color:' + color + '!important}';
+
+				if ($("iframe").contents().find("#customTheme").length > 0) {
+
+					$("iframe").contents().find("#customTheme").remove();
+				}
+
+				var theme = "<style>";
+				theme += '</style>';
+
+				$("iframe").contents().find("head").append(theme);
+				$("#pearadmin-bg-color").html(style);
 			}
 		};
 
@@ -224,15 +300,38 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 
 		$("body").on("click", ".setting", function() {
 
-			openRight();
+			var bgColorHtml =
+				'<li class="layui-this" data-select-bgcolor="dark-theme" >' +
+				'<a href="javascript:;" data-skin="skin-blue" style="" class="clearfix full-opacity-hover">' +
+				'<div><span style="display:block; width: 20%; float: left; height: 12px; background: #28333E;"></span><span style="display:block; width: 80%; float: left; height: 12px; background: white;"></span></div>' +
+				'<div><span style="display:block; width: 20%; float: left; height: 40px; background: #28333E;"></span><span style="display:block; width: 80%; float: left; height: 40px; background: #f4f5f7;"></span></div>' +
+				'</a>' +
+				'</li>';
 
-		})
-		
-		function openRight() {
-		    var themeHtml =	buildThemeHtml();
-			
-			var linkHtml = buildLinkHtml();
-			
+			bgColorHtml +=
+				'<li  data-select-bgcolor="light-theme" >' +
+				'<a href="javascript:;" data-skin="skin-blue" style="" class="clearfix full-opacity-hover">' +
+				'<div><span style="display:block; width: 20%; float: left; height: 12px; background: white;"></span><span style="display:block; width: 80%; float: left; height: 12px; background: white;"></span></div>' +
+				'<div><span style="display:block; width: 20%; float: left; height: 40px; background: white;"></span><span style="display:block; width: 80%; float: left; height: 40px; background: #f4f5f7;"></span></div>' +
+				'</a>' +
+				'</li>';
+            
+			bgColorHtml +=
+				'<li  data-select-bgcolor="night-theme" >' +
+				'<a href="javascript:;" data-skin="skin-blue" style="" class="clearfix full-opacity-hover">' +
+				'<div><span style="display:block; width: 20%; float: left; height: 12px; background: #28333E;"></span><span style="display:block; width: 80%; float: left; height: 12px; background: #28333E;"></span></div>' +
+				'<div><span style="display:block; width: 20%; float: left; height: 40px; background: #28333E;"></span><span style="display:block; width: 80%; float: left; height: 40px; background: #28333E;"></span></div>' +
+				'</a>' +
+				'</li>';
+
+			var html =
+				'<div class="pearone-color">\n' +
+				'<div class="color-title">整体风格</div>\n' +
+				'<div class="color-content">\n' +
+				'<ul>\n' + bgColorHtml + '</ul>\n' +
+				'</div>\n' +
+				'</div>';
+
 			layer.open({
 				type: 1,
 				offset: 'r',
@@ -244,11 +343,28 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 				anim: -1,
 				skin: 'layer-anim-right',
 				move: false,
-				content: themeHtml + linkHtml,
+				content: html + buildColorHtml() + buildLinkHtml(),
 				success: function(layero, index) {
 
 					form.render();
 
+					var color = localStorage.getItem("theme-color");
+					var menu = localStorage.getItem("theme-menu");
+					
+					if (color != "null") {
+						$(".select-color-item").removeClass("layui-icon")
+							.removeClass("layui-icon-ok");
+
+						$("*[color-id='" + color + "']").addClass("layui-icon")
+							.addClass("layui-icon-ok");
+					}
+					
+					if (menu!="null"){
+						$("*[data-select-bgcolor]").removeClass("layui-this");
+						
+						$("[data-select-bgcolor='"+menu+"']").addClass("layui-this");
+					}
+					
 					$('#layui-layer-shade' + index).click(function() {
 						var $layero = $('#layui-layer' + index);
 						$layero.animate({
@@ -259,9 +375,9 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 					})
 				}
 			});
-		}
+		})
 
-		/** 同 步 请 求 获 取 数 据 */
+
 		function getData() {
 
 			$.ajaxSettings.async = false;
@@ -274,73 +390,76 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 			$.ajaxSettings.async = true;
 			return data;
 		}
-		
+
 		$('body').on('click', '[data-select-bgcolor]', function() {
+
 			var theme = $(this).attr('data-select-bgcolor');
 			$('[data-select-bgcolor]').removeClass("layui-this");
 			$(this).addClass("layui-this");
-			setColor(getThemeById(theme));
+			localStorage.setItem("theme-menu", theme);
+			pearAdmin.menuSkin(theme);
+
 		});
-		
-		function getThemeById(id){
+
+		$('body').on('click', '.select-color-item', function() {
+
+			$(".select-color-item").removeClass("layui-icon").removeClass("layui-icon-ok");
+			$(this).addClass("layui-icon").addClass("layui-icon-ok");
+			var colorId = $(".select-color-item.layui-icon-ok").attr("color-id");
+			localStorage.setItem("theme-color", colorId);
+			var color = getColorById(colorId);
+			pearAdmin.colorSet(color.color);
+		});
+
+
+		function getThemeById(id) {
 			var theme;
 			$.each(getData().theme, function(i, value) {
-				if(value.id == id){
+				if (value.id == id) {
 					theme = value;
 				}
 			})
 			return theme;
 		}
 
-        function setColor(theme){
-			  
-			   console.log("传过来的主题"+JSON.stringify(theme));
-		
-				var theme = "<style>";
-			
-				theme += '</style>';
-			
-				$("#pearadmin-bg-color").html(style);
-				
-		}
+		function getColorById(id) {
+			var color;
+			$.each(getData().colors, function(i, value) {
 
-		function buildThemeHtml() {
-
-			var bgColorHtml = "";
-
-			$.each(getData().theme, function(i, value) {
-
-				bgColorHtml += '<li  data-select-bgcolor="'+value.id+'">' +
-					'<a href="javascript:;" data-skin="skin-blue" style="" class="clearfix full-opacity-hover">' +
-					'<div><span style="display:block; width: 20%; float: left; height: 12px; background: '+value.logoBgColor+';"></span><span style="display:block; width: 80%; float: left; height: 12px; background: '+value.headerBgColor+';"></span></div>' +
-					'<div><span style="display:block; width: 20%; float: left; height: 40px; background: '+value.menuBgColor+';"></span><span style="display:block; width: 80%; float: left; height: 40px; background: #f4f5f7;"></span></div>' +
-					'</a>' +
-					'</li>';
+				if (value.id == id) {
+					color = value;
+				}
 			})
-
-			var html =
-				'<div class="pearone-color">\n' +
-				'<div class="color-title">整体风格</div>\n' +
-				'<div class="color-content">\n' +
-				'<ul>\n' + bgColorHtml + '</ul>\n' +
-				'</div>\n' +
-				'</div>';
-
-			return html;
+			return color;
 		}
-		
-		function buildLinkHtml(){
-			
+
+		function buildLinkHtml() {
+
 			var links = "";
-			
+
 			$.each(getData().links, function(i, value) {
-				
-				links += '<a class="more-menu-item" href="'+value.href+'" target="_blank">' +
-				'<i class="'+value.icon+'" style="font-size: 19px;"></i> '+value.title +
-				'</a>'
+
+				links += '<a class="more-menu-item" href="' + value.href + '" target="_blank">' +
+					'<i class="' + value.icon + '" style="font-size: 19px;"></i> ' + value.title +
+					'</a>'
 			})
-			
+
 			return '<div class="more-menu-list">' + links + '</div>';
+		}
+
+		function buildColorHtml() {
+
+			var colors = "";
+
+			$.each(getData().colors, function(i, value) {
+
+				colors += "<span class='select-color-item' color-id='" + value.id + "' style='background-color:" + value.color +
+					";'></span>";
+			})
+
+			return "<div class='select-color'><div class='select-color-title'>主题色</div><div class='select-color-content'>" +
+				colors + "</div></div>"
+
 		}
 
 		exports('admin', pearAdmin);
