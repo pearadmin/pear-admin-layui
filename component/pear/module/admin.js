e9f1fe6df2b30e7de1d2b1010028f2442e0484d8
就眠仪式 <854085467@qq.com>
2020-10-28 17:33:54 +0800

    解决问题

--- component/pear/module/admin.js
+++ component/pear/module/admin.js
@@ -24,12 +24,12 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 					});
 				}
 			}
-			
+
 			this.logoRender = function(param) {
 				$(".layui-logo .logo").attr("src", param.logo.image);
 				$(".layui-logo .title").html(param.logo.title);
 			}
-			
+
 			this.menuRender = function(param) {
 				sideMenu = pearMenu.render({
 					elem: 'sideMenu',
@@ -41,15 +41,15 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 					accordion: param.menu.accordion,
 					url: param.menu.data,
 					parseData: false,
-					change:function(){
+					change: function() {
 						compatible();
 					},
-					done:function(){
+					done: function() {
 						sideMenu.selectItem(param.menu.select);
 					}
 				});
 			}
-			
+
 			this.bodyRender = function(param) {
 				if (param.tab.muiltTab) {
 					bodyTab = pearTab.render({
@@ -71,8 +71,8 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 						}]
 					});
 					bodyTab.click(function(id) {
-						if(!param.tab.keepState){
-						   bodyTab.refresh(false);
+						if (!param.tab.keepState) {
+							bodyTab.refresh(false);
 						}
 						bodyTab.positionTab();
 						sideMenu.selectItem(id);
@@ -131,14 +131,14 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 					})
 				}
 			}
-			
+
 			this.keepLoad = function(param) {
 				compatible()
 				setTimeout(function() {
 					$(".loader-main").fadeOut(200);
 				}, param.other.keepLoad)
 			}
-			
+
 			this.themeRender = function(option) {
 				if (option.theme.allowCustom == false) {
 					$(".setting").remove();
@@ -158,13 +158,13 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 				this.colorSet(color.color);
 				this.menuSkin(menu);
 			}
-			
+
 			this.menuSkin = function(theme) {
 				$(".pear-admin").removeClass("light-theme");
 				$(".pear-admin").removeClass("dark-theme");
 				$(".pear-admin").addClass(theme);
 			}
-			
+
 			this.colorSet = function(color) {
 				var style = '';
 				// 自 定 义 菜 单 配 色
@@ -239,9 +239,9 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 				});
 			}
 		});
-		
-		$("body").on("click",'[user-menu-id]',function(){
-			if(config.tab.muiltTab){
+
+		$("body").on("click", '[user-menu-id]', function() {
+			if (config.tab.muiltTab) {
 				bodyTab.addTabOnly({
 					id: $(this).attr("user-menu-id"),
 					title: $(this).attr("user-menu-title"),
@@ -249,7 +249,7 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 					icon: "",
 					close: true
 				}, 300);
-			}else{
+			} else {
 				bodyFrame.changePage($(this).attr("user-menu-url"), "", true);
 			}
 		});
@@ -278,7 +278,7 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 				'<ul>\n' + bgColorHtml + '</ul>\n' +
 				'</div>\n' +
 				'</div>';
-				
+
 			layer.open({
 				type: 1,
 				offset: 'r',
@@ -293,7 +293,7 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 				content: html + buildColorHtml() + buildLinkHtml(),
 				success: function(layero, index) {
 					form.render();
-					
+
 					var color = localStorage.getItem("theme-color");
 					var menu = localStorage.getItem("theme-menu");
 
@@ -318,7 +318,7 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 				}
 			});
 		});
-		
+
 		$('body').on('click', '[data-select-bgcolor]', function() {
 			var theme = $(this).attr('data-select-bgcolor');
 			$('[data-select-bgcolor]').removeClass("layui-this");
@@ -326,7 +326,7 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 			localStorage.setItem("theme-menu", theme);
 			pearAdmin.menuSkin(theme);
 		});
-		
+
 		$('body').on('click', '.select-color-item', function() {
 			$(".select-color-item").removeClass("layui-icon").removeClass("layui-icon-ok");
 			$(this).addClass("layui-icon").addClass("layui-icon-ok");
@@ -335,16 +335,16 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 			var color = getColorById(colorId);
 			pearAdmin.colorSet(color.color);
 		});
-		
-		function readConfig(){
-		    var defer = $.Deferred();
+
+		function readConfig() {
+			var defer = $.Deferred();
 			$.getJSON("pear.config.json?fresh=" + Math.random(), function(result) {
-				 defer.resolve(result)
+				defer.resolve(result)
 			});
-		    return defer.promise();
+			return defer.promise();
 		}
 
-		function applyConfig(param){
+		function applyConfig(param) {
 			config = param;
 			pearAdmin.logoRender(param);
 			pearAdmin.menuRender(param);
@@ -391,7 +391,7 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 			return "<div class='select-color'><div class='select-color-title'>主题色</div><div class='select-color-content'>" +
 				colors + "</div></div>"
 		}
-		
+
 		function compatible() {
 			if ($(window).width() <= 768) {
 				sideMenu.collaspe();
@@ -406,7 +406,7 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 				}
 			}
 		}
-		
+
 		function screenFun(num) {
 			num = num || 1;
 			num = num * 1;
@@ -435,8 +435,9 @@ layui.define(['table', 'jquery', 'element', 'form', 'tab', 'menu', 'frame'],
 					}
 					break;
 			}
+			return new Promise(function(res, rej) {
+				res("返回值");
+			});
 		}
-		
 		exports('admin', pearAdmin);
 	})
-	
