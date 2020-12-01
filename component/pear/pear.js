7f4d39d27febd256bbdf810fa10ad63b926927b8
就眠仪式 <854085467@qq.com>
2020-12-01 12:47:25 +0800

    :bug: 修复样式源码文件

--- component/pear/pear.js
+++ component/pear/pear.js
@@ -24,21 +24,21 @@ layui.config({
 	drawer: "drawer",	// 抽屉弹层组件
 	notice: "notice",	// 消息提示组件
 	step:"step",		// 分布表单组件
-	tag:"tag",
-	popup:"popup",
-	iconPicker:"iconPicker",
-	treetable:"treetable",
-	dtree:"dtree",
-	tinymce:"tinymce/tinymce",
-	area:"area",
-	count:"count",
-	topBar: "topBar",
-	button: "button",
-	design: "design",
-	dropdown: "dropdown",
-	card: "card",
-	loading: "loading",
-	cropper:"cropper",
-	convert:"convert",
-	context:"context"
+	tag:"tag",			// 多标签页组件
+	popup:"popup",      // 弹层封装
+	iconPicker:"iconPicker", // 图表选择
+	treetable:"treetable",   // 树状表格
+	dtree:"dtree",			// 树结构
+	tinymce:"tinymce/tinymce", // 编辑器
+	area:"area",			// 省市级联
+	count:"count",			// 数字滚动组件
+	topBar: "topBar",		// 置顶组件
+	button: "button",		// 加载按钮
+	design: "design",		// 表单设计
+	dropdown: "dropdown",	// 下拉组件
+	card: "card",			// 数据卡片组件
+	loading: "loading",		// 加载组件
+	cropper:"cropper",		// 裁剪组件
+	convert:"convert",		// 数据转换
+	context:"context"		// 上下文组件
 });
