f919b7c6aa07bbcc518ff354b0bd48bb2a41790b
就眠仪式 <854085467@qq.com>
2021-02-04 13:38:04 +0800

    remove debug code

--- view/echarts/script/column2.js
+++ view/echarts/script/column2.js
@@ -47,7 +47,6 @@ layui.use(['echarts'], function() {
 	        show:true,
 	        position:'top',
 	        formatter:function(params){
-	            debugger
 	            return params.value+'%'
 	        }
 	    },
