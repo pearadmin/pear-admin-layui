419b2ca9a82f93d9786ce3bc8b912e8ef56b2b80
就眠仪式 <854085467@qq.com>
2021-03-10 23:05:40 +0800

    Unify the global button theme

--- component/pear/modules/theme.js
+++ component/pear/modules/theme.js
@@ -65,7 +65,7 @@ layui.define(["jquery","layer"], function (exports) {
 		style += 'div[xm-select-skin=normal] dl dd:not(.xm-dis-disabled) i{border-color:'+color+'!important}'
 		style += 'div[xm-select-skin=normal] dl dd.xm-select-this:not(.xm-dis-disabled) i{color:'+color+'!important}'
 		style += 'div[xm-select-skin=normal].xm-form-selected .xm-select, div[xm-select-skin=normal].xm-form-selected .xm-select:hover{border-color:'+color+'!important}'
-		style += '.layui-layer-btn a:first-child{border-color:'+color+';background-color:'+color+'!important}'
+		style += '.layui-layer-btn a:first-child{border-color:'+color+';background-color:'+color+'!important}';
 
 		var colorPane = $("#pear-admin-color");
 		if(colorPane.length>0){
