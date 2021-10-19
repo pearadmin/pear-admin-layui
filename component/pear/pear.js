efb5e73e174f0b1d19fce67743547a0b05cbbed6
就眠仪式 <854085467@qq.com>
2021-10-19 09:47:42 +0800

    🌺 publish version to 3.9.1

--- component/pear/pear.js
+++ component/pear/pear.js
@@ -5,7 +5,7 @@ window.rootPath = (function(src) {
 
 layui.config({
 	base: rootPath + "module/",
-	version: "3.9.0"
+	version: "3.9.1"
 }).extend({
 	admin: "admin", 	// 框架布局组件
 	menu: "menu",		// 数据菜单组件
