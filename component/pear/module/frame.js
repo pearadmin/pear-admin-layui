efe29f16b73307f60da9c0ac9937322b69227c55
gzy <gzyonline@hotmail.com>
2022-04-02 16:13:02 +0800

    优化：统一缩进符

--- component/pear/module/frame.js
+++ component/pear/module/frame.js
@@ -1,13 +1,13 @@
 layui.define(['jquery', 'element'], function (exports) {
-    "use strict";
+		"use strict";
 
-    var $ = layui.jquery;
+		var $ = layui.jquery;
 
-    var pearFrame = function (opt) {
-        this.option = opt;
-    };
+		var pearFrame = function (opt) {
+				this.option = opt;
+		};
 
-    pearFrame.prototype.render = function (opt) {
+		pearFrame.prototype.render = function (opt) {
 		var option = {
 			elem:opt.elem,
 			url:opt.url,
@@ -16,11 +16,11 @@ layui.define(['jquery', 'element'], function (exports) {
 			height:opt.height,
 			done:opt.done ? opt.done: function(){ console.log("菜单渲染成功");}
 		}
-	    createFrameHTML(option);
-	    $("#"+option.elem).width(option.width);
-	    $("#"+option.elem).height(option.height);
+			createFrameHTML(option);
+			$("#"+option.elem).width(option.width);
+			$("#"+option.elem).height(option.height);
 		return new pearFrame(option);
-    }
+		}
 
 	pearFrame.prototype.changePage = function(url,loading){
 		if(loading){
@@ -28,12 +28,12 @@ layui.define(['jquery', 'element'], function (exports) {
 			loading.css({display:'block'});
 		}
 		$("#"+this.option.elem+" iframe").attr("src",url);
-	     if(loading){
-	     	var loading = $("#"+this.option.elem).find(".pear-frame-loading");
+			 if(loading){
+			 	var loading = $("#"+this.option.elem).find(".pear-frame-loading");
 			setTimeout(function(){
 				loading.fadeOut(500);
 			},800)
-	     }
+			 }
 	}
 
 	pearFrame.prototype.changePageByElement = function(elem,url,title,loading){
@@ -42,13 +42,13 @@ layui.define(['jquery', 'element'], function (exports) {
 			loading.css({display:'block'});
 		}
 		$("#"+elem+" iframe").attr("src",url);
-	    $("#"+elem+" .title").html(title);
-	     if(loading){
-	     	var loading = $("#"+elem).find(".pear-frame-loading");
+			$("#"+elem+" .title").html(title);
+			 if(loading){
+			 	var loading = $("#"+elem).find(".pear-frame-loading");
 			setTimeout(function(){
 				loading.css({display:'none'});
 			},400)
-	     }
+			 }
 	}
 
 	pearFrame.prototype.refresh = function (time) {
@@ -66,12 +66,12 @@ layui.define(['jquery', 'element'], function (exports) {
 
 	function createFrameHTML(option){
 		 var iframe = "<iframe class='pear-frame-content' style='width:100%;height:100%;'  scrolling='auto' frameborder='0' src='"+option.url+"' ></iframe>";
-	     var loading = '<div class="pear-frame-loading">'+
-			       '<div class="ball-loader">'+
-				      '<span></span><span></span><span></span><span></span>'+
-			       '</div>'+
-		        '</div></div>';
-	     $("#"+option.elem).html("<div class='pear-frame'>"+ iframe + loading +"</div>");
+			 var loading = '<div class="pear-frame-loading">'+
+						 '<div class="ball-loader">'+
+							'<span></span><span></span><span></span><span></span>'+
+						 '</div>'+
+						'</div></div>';
+			 $("#"+option.elem).html("<div class='pear-frame'>"+ iframe + loading +"</div>");
 	}
 
 	exports('frame', new pearFrame());
