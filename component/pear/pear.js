a279e96b33a7f40743ac916a4034b914ea4d0041
sight <1453017105@qq.com>
2022-05-17 11:35:00 +0800

    fix: rootPath

--- component/pear/pear.js
+++ component/pear/pear.js
@@ -1,5 +1,7 @@
 window.rootPath = (function (src) {
-	src = document.scripts[document.scripts.length - 1].src || document.currentScript.src;
+	src = document.currentScript
+		? document.currentScript.src
+		: document.scripts[document.scripts.length - 1].src;
 	return src.substring(0, src.lastIndexOf("/") + 1);
 })();
 layui.config({
